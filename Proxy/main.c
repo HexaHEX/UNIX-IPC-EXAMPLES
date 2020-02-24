2586a1f043c7717fa2f322011ee1af3e1d8e3d80
Andrei Andreev <andreev.aa@phystech.edu>
2020-02-24 17:36:10 +0300

    Working on proxy example

--- Proxy/main.c
+++ Proxy/main.c
@@ -8,7 +8,7 @@
 #include <sys/types.h>
 #include <sys/select.h>
 
-#define BUFFSIZE 27*(1 << 10)
+#define BUFFSIZE 27*(1 << 9)
 
 
 void err_sys(const char* error);
@@ -59,11 +59,11 @@ int main(int argc, char* argv[])
 		fcntl(client_structs[i].fds[3], F_SETFL, O_WRONLY|O_NONBLOCK);
 	}
 
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i < n-1; i++)
 	{
 		if(fork == 0)
 		{
-			for (int j = 0; j < n; j++)
+			for (int j = 1; j < n-1; j++)
 			{
 				if(i != j)
 				{
@@ -96,11 +96,104 @@ int main(int argc, char* argv[])
 			close(out);
 			exit(0);
 		}
-		else
-		{
+	}
+	for(int i = 1; i < n-1; i++)
+	{
+		close(client_structs[i].fds[0]);
+		close(client_structs[i].fds[3]);
+	}
 
+	int mul = 1;
+	int maxfd = -1;
+
+	for(int i = n-1; i > 0; i--)
+	{
+		client_structs[i].capacity = mul * 1 << 9;
+		client_structs[i].freedata = (char*)calloc(capacity, sizeof(char));
+		client_structs[i].data = client_structs[i].freedata;
+		client_structs[i].size = 0;
+
+		if(mul <= 27)
+		{
+			mul *= 3;
 		}
 	}
+
+	while (true)
+	{
+		fd_set readable;
+		fd_set writeable;
+		int fds = 0;
+
+		FD_ZERO(&readable);
+		FD_ZERO(&writeable);
+
+		for(int i = 0; i < n; i++)
+		{
+			if(client_structs[i].fds[1] > maxfd)
+				maxfd = client_structs[i].fds[1];
+
+			if(client_structs[i].fds[2] > maxfd)
+				maxfd = client_structs[i].fds[2];
+
+			if((client_structs[i].fds[2] != -1) && (client_structs[i+1].size == 0))
+			{
+				FD_SET(client_structs[i].fds[2], &readable);
+				fds++;
+			}
+
+			if((client_structs[i].fds[1] != -1) && (client_structs[i].size != 0))
+			{
+				FD_SET(client_structs[i].fds[1], &writeable);
+				fds++;
+			}
+		}
+
+		if(fds == 0)
+			break;
+		int seletret = select(maxfd + 1, &readable, &writeable, NULL, NULL);
+
+		if(selectret == 0)
+			break;
+		if(selectret == -1)
+			err_sys("SELECT ERROR");
+
+		for(int i = 0; i < n; i++)
+		{
+			if(FD_ISSET(client_structs[i].fds[2], &readable))
+			{
+				int readret = read(client_structs[i].fds[2], client_structs[i+1].data, client_structs[i+1].capacity);
+
+				if(readret == -1)
+					err_sys("READ ERROR");
+				if(readret == 0)
+				{
+					close(client_structs[i].fds[2]);
+					close(client_structs[i+1].fds[1]);
+					client_structs[i].fds[2] = client_structs[i+1].fds[1] = -1;
+				}
+			}
+
+			if(FD_ISSET(client_structs[i].fds[1], &writeable))
+			{
+				int writeret = write(client_structs[i].fds[1], client_structs[i+1].freedata, client_structs[i+1].size);
+
+				if(writeret == -1)
+					err_sys("READ ERROR");
+				if(writeret < client_structs[i].size)
+					client_structs[i].freedata += writeret;
+				else
+					client_structs[i].freedata = client_structs[i].data;
+				client_structs[i].size -= writeret;
+			}
+		}
+	}
+	for(int i = 1; i < n; i++)
+	{
+		free(client_structs[i].data);_
+	}
+	free(client_structs);
+	exit(0);
 }
 
 void err_sys(const char* error)
